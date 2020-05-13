6dd727167d3340b698da474a6ffdeb3981057176
Craig Cheney <ccheney@majesticLabs.com>
2020-05-12 21:41:07 -0400

    Add MS5607 Absolute pressure sensor

---
+++ include/MS5607.h
@@ -0,0 +1,107 @@
+/***************************************************************************
+*                              Majestic Labs  © 2020
+*                               
+*
+* File: MS5607.h
+* Workspace: micaOS
+* Project: Drivers
+* Version: 1.0.0
+* Authors: C. Cheney
+* 
+* PCB: N/A 
+*
+* Brief:
+*   Header for MS5607.c
+*
+* 2020.05.12  - Document Created
+********************************************************************************/
+
+/* Header Guard */
+#ifndef MS5607_H
+  #define MS5607_H
+  /***************************************
+  * Included files
+  ***************************************/
+  #include "micaComms.h"
+  
+  /***************************************
+  * Macro Definitions
+  ***************************************/
+  #define MS5607_CMD_RESET       (0x1E)  /* Reset the device */
+  #define MS5607_CMD_D1_256      (0x40)  /* Begin the D1 conversion at an OSR of 256 */
+  #define MS5607_CMD_D1_512      (0x42)  /* Begin the D1 conversion at an OSR of 512 */
+  #define MS5607_CMD_D1_1024     (0x44)  /* Begin the D1 conversion at an OSR of 1024 */
+  #define MS5607_CMD_D1_2048     (0x46)  /* Begin the D1 conversion at an OSR of 2048 */
+  #define MS5607_CMD_D1_4096     (0x48)  /* Begin the D1 conversion at an OSR of 4096 */
+  #define MS5607_CMD_D2_256      (0x50)  /* Begin the D2 conversion at an OSR of 256 */
+  #define MS5607_CMD_D2_512      (0x52)  /* Begin the D2 conversion at an OSR of 512 */
+  #define MS5607_CMD_D2_1024     (0x54)  /* Begin the D2 conversion at an OSR of 1024 */
+  #define MS5607_CMD_D2_2048     (0x56)  /* Begin the D2 conversion at an OSR of 2048 */
+  #define MS5607_CMD_D2_4096     (0x58)  /* Begin the D2 conversion at an OSR of 4096 */
+  #define MS5607_CMD_ADC         (0x00)  /* Read the ADC*/
+
+  #define MS5607_ADDR_PROM        (0xA0)  /* Address of the  the C1 Coefficient*/
+  // #define MS5607_CMD_PROM_C1     (0xA0)  /* Read the C1 Coefficient*/
+  // #define MS5607_CMD_PROM_C2     (0xA2)  /* Read the C2 Coefficient*/
+  // #define MS5607_CMD_PROM_C3     (0xA4)  /* Read the C3 Coefficient*/
+  // #define MS5607_CMD_PROM_C4     (0xA6)  /* Read the C4 Coefficient*/
+  // #define MS5607_CMD_PROM_C5     (0xA8)  /* Read the C5 Coefficient*/
+  // #define MS5607_CMD_PROM_C6     (0xAE)  /* Read the C6 Coefficient*/
+
+  #define MS5607_COEFFS_LEN     (6)
+  /***************************************
+  * Enumerated Types
+  ***************************************/
+  typedef enum {
+    MS5607_OSR_256,     /* Conversion time 0.54 ms */
+    MS5607_OSR_512,     /* Conversion time 1.06 ms */
+    MS5607_OSR_1024,    /* Conversion time 2.08 ms */
+    MS5607_OSR_2048,    /* Conversion time 4.13 ms */
+    MS5607_OSR_4096     /* Conversion time 8.22 ms */
+  } MS5607_OSR_T;
+  /* Which variable to read */
+  typedef enum {
+    MS5607_PRESSURE,
+    MS5607_TEMP,
+  }MS5607_DATA_T;
+  
+  /***************************************
+  * Structures
+  ***************************************/
+  typedef struct {
+    uint32_t error;         /**< Functions that exit with an error use this to transmit info out */
+    int32_t temp;         /* Calculated temperature */
+    int32_t pressure;     /* Calculated pressure */
+    COMMS_I2C_S *_i2c;     /**< Pointer to the I2C comms struct */      
+    uint8_t _deviceAddr;     /**< Address of the device to communicate with */
+    MS5607_DATA_T _convertType; /* The data type from the last read  */
+    uint16_t _coeffs[MS5607_COEFFS_LEN];
+    // uint16_t _C1;         /**< Pressure sensitivity | SENST1 */
+    // uint16_t _C2;         /**< Pressure offset | OFFT1 */
+    // uint16_t _C3;         /**< Temperature coefficient of pressure sensitivity | TCS */
+    // uint16_t _C4;         /**< Temperature coefficient of pressure offset | TCO */
+    // uint16_t _C5;         /**< Reference temperature | TREF */
+    // uint16_t _C6;         /**< Temperature coefficient of the temperature | TEMPSENS */
+    // uint32_t _D1;         /**< Digital Pressure Value */
+    // uint32_t _D2;         /**< Digital Temperature Value */
+
+  } MS5607_STATE_S;
+  
+  /***************************************
+  * Function declarations 
+  ***************************************/
+  uint32_t MS5607_start(MS5607_STATE_S *const state, COMMS_I2C_S *const i2c, uint8_t i2cAddr);
+  uint32_t MS5607_reset(MS5607_STATE_S *const state);
+  uint32_t MS5607_startConvert(MS5607_STATE_S *const state, MS5607_DATA_T convertType, MS5607_OSR_T osr);
+  uint32_t MS5607_read_ADC(MS5607_STATE_S *const state);
+  uint32_t MS5607_read_ROM(MS5607_STATE_S *const state);
+  uint32_t MS5607_calculatePressureTemp(MS5607_STATE_S *const state);
+
+  uint32_t MS5607_writeCmd(MS5607_STATE_S *const state, uint8_t cmd);
+
+
+
+
+
+#endif /* MS5607_H */
+/* [] END OF FILE */
---
+++ src/MS5607.c
@@ -0,0 +1,202 @@
+/***************************************************************************
+*                              Majestic Labs  © 2020
+*                               
+*
+* File: MS5607.c
+* Workspace: micaOS
+* Project: Drivers
+* Version: 1.0.0
+* Authors: C. Cheney
+* 
+* PCB: N/A
+* mcuType: Generic 
+* partNumber:MS560702BA03-50
+*
+* Brief:
+*   Driver file for the MS5607 Pressure Sensor
+*
+* 2020.05.12  - Document Created
+********************************************************************************/
+#include "MS5607.h"
+#include "micaCommon.h"
+
+/*******************************************************************************
+* Function Name: MS5607_start()
+****************************************************************************//**
+* \brief
+*   Starts the MS5607 Pressure sensor. 
+*
+* \param state [in/out]
+* Pointer to the state struct
+*
+* \param i2c [in]
+* Pointer to the i2c comms struct
+*
+* \param i2cAddr [in]
+* Value of the device I2C Address
+*  
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t MS5607_start(MS5607_STATE_S *const state, COMMS_I2C_S *const i2c, uint8_t i2cAddr){
+  /* Validate and assign */
+  uint32_t error = COMMS_ERROR_NONE;
+  state->_deviceAddr = i2cAddr;
+  state->_i2c = i2c;
+  error |= Comms_validateI2C(state->_i2c);
+  if(!error) {
+    /* Reset device */
+    error |= MS5607_reset(state);
+    /* Ensure read byte matches reset error */
+    /* read the PROM */
+    error |= MS5607_read_ROM(state);
+  }
+  else {
+    error |= COMMS_ERROR_START; 
+  }
+
+  return error;
+}
+
+/*******************************************************************************
+* Function Name: MS5607_reset
+****************************************************************************//**
+* \brief
+*   Send the Reset command to the Pressure sensor 
+*
+* \param state [in/out]
+* Pointer to the state struct
+*  
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t MS5607_reset(MS5607_STATE_S *const state) {
+  return MS5607_writeCmd(state, MS5607_CMD_RESET);
+}
+
+/*******************************************************************************
+* Function Name: MS5607_read_ROM
+****************************************************************************//**
+* \brief
+*   Reads all 6 parameters from the ROM 
+*
+* \param state [in/out]
+* Pointer to the state struct
+*  
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t MS5607_read_ROM(MS5607_STATE_S *const state){
+  /* Ensure Comms exists */
+  uint32_t error = Comms_validateI2C(state->_i2c);
+  if(error){
+    /* Store the comms error reported */
+    state->error = error;
+    error = COMMS_ERROR_I2C;
+  /* Proceed with write */
+  } else {
+    /* Read each Coefficient */
+    uint8_t C;
+    for(C=ZERO; C < MS5607_COEFFS_LEN; C++) {
+      uint8_t readArray[TWO];
+      uint8_t cAddr = MS5607_ADDR_PROM + (C * TWO);
+      uint32_t i2cError = state->_i2c->readArray(state->_deviceAddr, cAddr, readArray, TWO);
+
+      /* Error check */
+      error |= i2cError ? COMMS_ERROR_I2C : COMMS_ERROR_NONE;
+      if (!error) {
+        uint16_t MSDB = readArray[ZERO];
+        uint16_t LSDB = readArray[ONE];
+        state->_coeffs[C] = (MSDB << SHIFT_BYTE_ONE) |  LSDB;
+      } else {
+        state->error = i2cError;
+        break;
+      }
+    }
+  }
+  return error;
+}
+
+
+/*******************************************************************************
+* Function Name: MS5607_writeCmd
+****************************************************************************//**
+* \brief
+*   Write a register over the MS5607 I2C bus
+*
+* \param state [in/out]
+* Pointer to the state struct
+*
+* \param cmd [in]
+*   Address of the register to write to
+*
+*  
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t MS5607_writeCmd(MS5607_STATE_S *const state, uint8_t cmd){
+  /* Ensure Comms exists */
+  uint32_t error = Comms_validateI2C(state->_i2c);
+  if(error){
+    /* Store the comms error reported */
+    state->error = error;
+    error = COMMS_ERROR_I2C;
+  /* Proceed with write */
+  } 
+  else {
+    uint32_t i2cError = state->_i2c->writeCmd(state->_deviceAddr, cmd);
+    /* Place output error in the state struct */
+    error |= i2cError ? COMMS_ERROR_I2C : COMMS_ERROR_NONE;
+    if (error) {
+      state->error = i2cError;
+    }
+  }
+  return error;
+}
+
+
+/*******************************************************************************
+* Function Name: MS5607_read()
+****************************************************************************//**
+* \brief
+*   Read a register over the MS5607 I2C bus
+*
+* \param state [in/out]
+* Pointer to the state struct
+*
+* \param regAddr [in]
+*   Address of the register to write to
+*
+* \param val [out]
+*   Pointer where to place the result
+*  
+* \return
+*  Error code of the operation
+*******************************************************************************/
+uint32_t MS5607_read(MS5607_STATE_S *const state, uint8_t regAddr, uint8_t* val){
+  /* Ensure Comms exists */
+  uint32_t error = Comms_validateI2C(state->_i2c);
+  if(error){
+    /* Store the comms error reported */
+    state->error = error;
+    error = COMMS_ERROR_I2C;
+  /* Proceed with write */
+  } else {
+    uint8_t readArray[TWO];
+    uint32_t i2cError = state->_i2c->readArray(state->_deviceAddr, regAddr, readArray, TWO);
+    /* Write out */
+    uint16_t MSDB = readArray[ZERO];
+    uint16_t LSDB = readArray[ONE];
+    *val = (MSDB << SHIFT_BYTE_ONE) |  LSDB ;
+    error |= i2cError ? COMMS_ERROR_I2C : COMMS_ERROR_NONE;
+    if (error) {
+      state->error = i2cError;
+    }
+  }
+  return error;
+}
+
+
+
+
+/* [] END OF FILE */
